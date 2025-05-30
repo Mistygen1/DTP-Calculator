8ac8fed3ccf60295904b1edf4ef96ba56f55f1b2
Patryk S <45711351+Mistygen1@users.noreply.github.com>
2025-05-30 18:49:24 +0200

    Update main.js

--- main.js
+++ main.js
@@ -4,6 +4,6 @@ let dpcost = document.getElementById("dpcost");
 const result = document.getElementById("result")
 
 const calculateTime = () =>{
-    const timeInHouers = (dpcost.value / dps.value)/60/60;
+    const timeInHouers = ((dpcost.value / dps.value)/60/60).toFixed(2);
     result.innerHTML = "Time in houers: " + timeInHouers
 }
