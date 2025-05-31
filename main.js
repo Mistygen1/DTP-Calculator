6c4b16bf23416f0147fd5adbfaa735f2019fbe29
Patryk S <45711351+Mistygen1@users.noreply.github.com>
2025-05-31 11:18:48 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,27 @@
 let dps = document.getElementById("dps");
 let dpcost = document.getElementById("dpcost");
+let points = document.getElementById("points")
 
 const result = document.getElementById("result")
+const genExp = document.getElementById("genPowerExp")
+const commExp = document.getElementById("commonExp")
 
-const calculateTime = () =>{
-    const timeInHouers = ((dpcost.value / dps.value)/60/60).toFixed(2);
+const calculateTime = () => {
+    const timeInHouers = (dpcost.value / dps.value)/60/60;
     result.innerHTML = "Time in houers: " + timeInHouers
+}
+
+const calculateLabPoints = () => {
+    let currPoints = points.value;
+    let genExpPoints;
+    let commExpPoints
+
+    for(let i = 1;i <= currPoints;i++){
+        if(i % 5){
+            genExpPoints++;
+        }
+        commExpPoints++;
+    }
+    genExp.innerHTML = "Points in gen power exponent: " + genExpPoints
+    commExp.innerHTML = "Points in common exponent: " + commExpPoints
 }
